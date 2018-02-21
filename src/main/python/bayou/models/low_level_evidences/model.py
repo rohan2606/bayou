a15d8d323614c564661829b04cbd0140c3b6d221
rm38 <rm38@cslinux20.cs.rice.edu>
2018-02-20 19:54:40 -0600

    Removed all bayesian components and used simpler settings in config.json

--- src/main/python/bayou/models/low_level_evidences/model.py
+++ src/main/python/bayou/models/low_level_evidences/model.py
@@ -66,17 +66,17 @@ class Model():
                                           , axis=1)
         self.KL_loss = KL_loss
 
-        # 3. Latent loss
-        latent_loss = 0.5 * tf.reduce_sum(- tf.log(self.reverse_encoder.psi_covariance)
-                                          - 1 + self.reverse_encoder.psi_covariance
-                                          + tf.square(self.reverse_encoder.psi_mean), axis=1)
-        self.latent_loss = config.alpha * latent_loss
-
-        # 3. evidence loss: log P(f(\theta) | \Psi; \sigma)
-        evidence_loss = [ev.evidence_loss(self.psi, encoding, config) for ev, encoding
-                         in zip(config.evidence, self.encoder.encodings)]
-        evidence_loss = [tf.reduce_sum(loss, axis=1) for loss in evidence_loss]
-        self.evidence_loss = config.beta * tf.reduce_sum(tf.stack(evidence_loss), axis=0)
+        # # 3. Latent loss
+        # latent_loss = 0.5 * tf.reduce_sum(- tf.log(self.reverse_encoder.psi_covariance)
+        #                                   - 1 + self.reverse_encoder.psi_covariance
+        #                                   + tf.square(self.reverse_encoder.psi_mean), axis=1)
+        # self.latent_loss = config.alpha * latent_loss
+        #
+        # # 3. evidence loss: log P(f(\theta) | \Psi; \sigma)
+        # evidence_loss = [ev.evidence_loss(self.psi, encoding, config) for ev, encoding
+        #                  in zip(config.evidence, self.encoder.encodings)]
+        # evidence_loss = [tf.reduce_sum(loss, axis=1) for loss in evidence_loss]
+        # self.evidence_loss = config.beta * tf.reduce_sum(tf.stack(evidence_loss), axis=0)
 
         # The optimizer
         self.loss = self.gen_loss + self.KL_loss + self.latent_loss + self.evidence_loss
