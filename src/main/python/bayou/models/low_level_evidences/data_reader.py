710e7b26386da7ff9b6a47d0e3b67209af270ef8
Ubuntu <ubuntu@ip-172-16-0-246.ec2.internal>
2018-04-04 20:56:14 +0000

    added random sampling and used all data pts. This version was used to collect first RE model with fixed psi_covar is I/5.

--- src/main/python/bayou/models/low_level_evidences/data_reader.py
+++ src/main/python/bayou/models/low_level_evidences/data_reader.py
@@ -42,7 +42,7 @@ class Reader():
                          enumerate(config.evidence)]
 
         # align with number of batches
-        config.num_batches = 100 #int(len(raw_targets) / config.batch_size)
+        config.num_batches = int(len(raw_targets) / config.batch_size)
         assert config.num_batches > 0, 'Not enough data'
         sz = config.num_batches * config.batch_size
         for i in range(len(raw_evidences)):
@@ -206,8 +206,8 @@ class Reader():
 
         # randomly shuffle to avoid bias towards initial data points during training
         # COMMENTING THIS IS WRONG
-        print("Random Shuffle is turned off, TURN IT ON FOR FULL DATA TRAINING")
-#        random.shuffle(data_points)
+        #print("Random Shuffle is turned off, TURN IT ON FOR FULL DATA TRAINING")
+        random.shuffle(data_points)
 
 
         evidences, targets = zip(*data_points)
