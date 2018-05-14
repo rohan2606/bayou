f8c01a3f3b80cb64c209cc0f179f66adbb8a9529
Rohan <rm38@rice.edu>
2018-05-14 15:51:38 -0500

    added the config with good accuracy

--- src/main/python/bayou/models/low_level_evidences/data_reader.py
+++ src/main/python/bayou/models/low_level_evidences/data_reader.py
@@ -35,7 +35,7 @@ class Reader():
     def __init__(self, clargs, config):
         self.config = config
 
-        #random.seed(12)
+        random.seed(12)
         # read the raw evidences and targets
         print('Reading data file...')
         prog_ids, raw_evidences, raw_targets = self.read_data(clargs.input_file[0],save=clargs.save)
@@ -44,7 +44,7 @@ class Reader():
 
 
         # align with number of batches
-        config.num_batches = int(len(raw_targets) / config.batch_size)
+        config.num_batches = 100 #int(len(raw_targets) / config.batch_size)
         assert config.num_batches > 0, 'Not enough data'
         sz = config.num_batches * config.batch_size
         for i in range(len(raw_evidences)):
