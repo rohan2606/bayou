695c0c99b1c0dd9ca04fcda64be1bd4269dbb837
Rohan <rm38@rice.ed>
2019-11-10 05:59:59 +0000

    added code for data manip

---
+++ src/main/python/scripts/file_name_manip.py
@@ -0,0 +1,11 @@
+import os
+
+
+offset = 67
+for a in os.listdir():
+     if 'Program_output' in a:
+         number = int(a.split('.')[0].split('_')[-1])
+         number += offset 
+         replace_with = 'Program_output_' + str(number) + '.json' 
+         os.rename(a, replace_with)
+         print(f'Replacing {a} with {replace_with}' )
