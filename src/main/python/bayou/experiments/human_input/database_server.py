ede3664304aba5e73686886ea00c9a37c3a8f454
Rohan <rm38@rice.edu>
2019-11-18 06:59:02 +0000

    added db server to tester

---
+++ src/main/python/bayou/experiments/human_input/database_server.py
@@ -0,0 +1,106 @@
+import tensorflow as tf
+import bayou.models.low_level_evidences.predict
+import argparse
+import sys
+import socket
+import json
+import os
+
+from bayou.experiments.predictMethods.SearchDB.parallelReadJSON import parallelReadJSON
+from bayou.experiments.predictMethods.SearchDB.searchFromDB import searchFromDB
+from bayou.experiments.predictMethods.SearchDB.Embedding import EmbeddingBatch
+
+
+
+class Rev_Encoder_Model:
+    def __init__(self, batch_size=1, topK=10):
+        self.numThreads = 30
+        self.batch_size = batch_size
+        self.minJSONs = 1
+        self.maxJSONs =  10
+        self.dimension = 256
+        self.topK = topK
+        self.scanner = self.get_database_scanner()
+        return
+
+    def get_database_scanner(self):
+
+        JSONReader = parallelReadJSON('/home/ubuntu/DATABASE/', numThreads=self.numThreads, dimension=self.dimension, batch_size=self.batch_size, minJSONs=self.minJSONs , maxJSONs=self.maxJSONs)
+        listOfColDB = JSONReader.readAllJSONs()
+        scanner = searchFromDB(listOfColDB, self.topK, self.batch_size)
+        return scanner
+
+
+    def dump_result(self, all_qry_progs):
+        reverse_encoder_batch_top_progs = self.get_results(all_qry_progs)
+        for j, rev_encoder_top_progs in enumerate(reverse_encoder_batch_top_progs):
+            for i, top_prog in enumerate(rev_encoder_top_progs):
+               #print('Rank ::' +  str(i))
+               #print('Prob ::' + str(top_prog[1]))
+               #print(top_prog[0])
+               _folder = 'log/qry_' + str(j)
+               if not os.path.exists(_folder):
+                    os.makedirs(_folder)
+               with open(_folder + '/program'+str(i)+'.java','w') as f:
+                    f.write(top_prog[0])
+            print("=====================================")
+    
+        os.remove('log/output.json')
+
+        
+    def get_results(self, all_qry_progs):
+
+        embIt_json = []
+        for encA, encB in zip(all_qry_progs['eAs'], all_qry_progs['eBs']):
+             embIt_json.append({'a1':encA, 'b1':encB})
+
+        embIt_batch = EmbeddingBatch(embIt_json, self.batch_size, 256)
+        topKProgsBatch = self.scanner.searchAndTopKParallel(embIt_batch, numThreads = self.numThreads)
+        return [[(prog[0].body, prog[1]) for prog in topKProgs[:self.topK]] for topKProgs in topKProgsBatch]
+
+
+
+
+def socket_server(rev_encoder):
+	serv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	serv.bind((socket.gethostname(), 5001))
+	serv.listen(5)
+	print('Server Starting!')
+	while True:
+	     conn, addr = serv.accept()
+	     print('Client Accepted')
+	     #from_client = ''
+	     while True:
+	         data = conn.recv(1000000)
+	         data.decode()
+	         if not data: break
+	         #from_client += data
+	         
+	         results = rev_encoder.dump_result(json.loads(data))
+	         send_data='done'
+	         conn.send(send_data.encode())
+	conn.close()
+	print ('client disconnected')
+
+
+
+
+
+if __name__ == "__main__":
+    #%%
+    parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter)
+    parser.add_argument('--python_recursion_limit', type=int, default=10000,
+    help='set recursion limit for the Python interpreter')
+
+
+    clargs = parser.parse_args()
+    sys.setrecursionlimit(clargs.python_recursion_limit)
+
+
+    #rev_encoder = Rev_Encoder_Model_2(pred)
+    rev_encoder = Rev_Encoder_Model(batch_size=10, topK=5)
+    #rev_encoder_batch_top_progs = rev_encoder.get_result(eAs, eBs)
+    
+    socket_server(rev_encoder)
+    
+
--- src/main/python/bayou/experiments/human_input/main_tester.py
+++ src/main/python/bayou/experiments/human_input/main_tester.py
@@ -16,7 +16,6 @@
 
 from __future__ import print_function
 import numpy as np
-import tensorflow as tf
 
 import argparse
 import os
@@ -27,9 +26,6 @@ import re
 
 import time
 import ijson
-from bayou.experiments.predictMethods.SearchDB.parallelReadJSON import parallelReadJSON
-from bayou.experiments.predictMethods.SearchDB.searchFromDB import searchFromDB
-from bayou.experiments.predictMethods.SearchDB.Embedding import EmbeddingBatch
 
 
 import socket
@@ -56,36 +52,17 @@ class Java_Reader:
 
 
 
+def client_socket_database(st):
+	client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	client.connect((socket.gethostname(), 5001))
+	byt = st.encode()
+	client.send(byt)
+	from_server = client.recv(1000000)
+	from_server.decode()
+	client.close()
+	return from_server
 
 
-class Rev_Encoder_Model:
-    def __init__(self, batch_size=1, topK=10):
-        self.numThreads = 30
-        self.batch_size = batch_size
-        self.minJSONs = 1
-        self.maxJSONs =  10
-        self.dimension = 256
-        self.topK = topK
-        self.scanner = self.get_database_scanner()
-        return
-
-    def get_database_scanner(self):
-
-        JSONReader = parallelReadJSON('/home/ubuntu/DATABASE/', numThreads=self.numThreads, dimension=self.dimension, batch_size=self.batch_size, minJSONs=self.minJSONs , maxJSONs=self.maxJSONs)
-        listOfColDB = JSONReader.readAllJSONs()
-        scanner = searchFromDB(listOfColDB, self.topK, self.batch_size)
-        return scanner
-
-
-    def get_result(self, encAs, encBs):
-        
-        embIt_json = []
-        for encA, encB in zip(encAs, encBs):
-             embIt_json.append({'a1':encA, 'b1':encB})
-
-        embIt_batch = EmbeddingBatch(embIt_json, self.batch_size, 256)
-        topKProgsBatch = self.scanner.searchAndTopKParallel(embIt_batch, numThreads = self.numThreads)
-        return [[(prog[0].body, prog[1]) for prog in topKProgs[:self.topK]] for topKProgs in topKProgsBatch]
 
 
 def client_socket(st):
@@ -155,19 +132,11 @@ if __name__ == "__main__":
     	eAs.append(eA)
     	eBs.append(eB)
     	programs.append(program)
+    
 
+    all_qry_progs = {'eAs':eAs,'eBs':eBs}
+    server_data = client_socket_database(json.dumps(all_qry_progs))
+    server_data.decode()
+    print(server_data)
     #rev_encoder = Rev_Encoder_Model_2(pred)
-    rev_encoder = Rev_Encoder_Model(batch_size = len(eAs), topK=clargs.topK)
-    rev_encoder_batch_top_progs = rev_encoder.get_result(eAs, eBs)
 
-    for j, rev_encoder_top_progs in enumerate(rev_encoder_batch_top_progs):
-    	print(programs[j])
-    	for i, top_prog in enumerate(rev_encoder_top_progs):
-    	   print('Rank ::' +  str(i))
-    	   print('Prob ::' + str(top_prog[1]))
-    	   print(top_prog[0])
-    	   with open('log/qry_' + str(j) + '/program'+str(i)+'.java','w') as f:
-    	        f.write(top_prog[0])
-    	print("=====================================")
-
-    os.remove('log/output.json')
